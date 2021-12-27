942da260df6cb69eff547882b7129a99943d59c8
Nikhil <nikhilraj.086@gmail.com>
2021-12-27 15:50:48 +0530

    27/12/2021

--- Basic to Intermediate Python Projects/All_exercises/89.py
+++ Basic to Intermediate Python Projects/All_exercises/89.py
@@ -3,12 +3,10 @@
 import sqlite3
 
 conn = sqlite3.connect("database.db")
-c
-# cur = conn.cursor()
-# cur.execute("SELECT country FROM countries WHERE area >= 2000000")
-# rows = cur.fetchall()
-# conn.close()
-# print(rows)
-#
-# for i in rows:
-#     print(i[0])
+cur = conn.cursor()
+
+rows = cur.execute("SELECT * FROM countries WHERE area >= 2000000").fetchall()
+conn.close()
+
+for i in rows:
+    print(i)
